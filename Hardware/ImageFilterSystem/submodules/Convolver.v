0f62bdc8b701146b9ab56e93147304746782a1b3
t0d4 <todatakaaki@gmail.com>
2022-07-03 02:19:56 +0900

    fix: revised the file to be behaviorally described

--- Hardware/ImageFilterSystem/submodules/Convolver.v
+++ Hardware/ImageFilterSystem/submodules/Convolver.v
@@ -4,6 +4,7 @@ module Convolver #(
 ) (
     input
         clk,
+        reset,
         mode,
     input wire[BIT_PER_PIXEL-1:0]
         pixel_0,
@@ -15,25 +16,37 @@ module Convolver #(
         pixel_6,
         pixel_7,
         pixel_8,
-    output[7:0]
-        conv_out_binarized
+    output wire[BIT_PER_PIXEL-1:0]
+        conv_out
 );
 
-parameter TMP_WIRE_WIDTH = 10;  // 10 bit is the maximum required width to store the output
-wire[TMP_WIRE_WIDTH-1:0] conv_out_tmp;
-wire[BIT_PER_PIXEL-1:0] conv_out_binarized_tmp;
+parameter TMP_WIRE_WIDTH = 12;  // 12 bit is the maximum required width to store the output
+reg[TMP_WIRE_WIDTH-1:0] conv_out_tmp;
+reg[TMP_WIRE_WIDTH-1:0] conv_out_tmp_sum_of_positive;
+reg[TMP_WIRE_WIDTH-1:0] conv_out_tmp_sum_of_negative;
 
-assign conv_out_tmp = mode ?
-    pixel_1 + pixel_3 - 4*pixel_4 + pixel_5 + pixel_7 :
-    pixel_0 + pixel_1 + pixel_2 - pixel_6 - pixel_7 - pixel_8;
+// if conv_out_tmp is negative, conv_out will be zero.
+// otherwise, if conv_out_tmp is more than 255, conv_out will be 255.
+//            if not, conv_out_tmp is ensured to be within [0, 255] so conv_out will be conv_out_tmp itself.
+assign conv_out = (conv_out_tmp_sum_of_negative>conv_out_tmp_sum_of_positive) ? 8'h00 :
+                  (|conv_out_tmp[TMP_WIRE_WIDTH-1:8]) ? 8'hff : conv_out_tmp[BIT_PER_PIXEL-1:0];
 
-// set conv_out_binarized 1 if conv_out_tmp is larger than or equal to 2^8
-assign conv_out_binarized_tmp = (|conv_out_tmp[9:8]) ? 8'hff : 8'h0;
-
-DFlipFlop #(.NUM_BITS(8)) d_ff_out (
-    .clk (clk),
-    .D (conv_out_binarized_tmp),
-    .Q (conv_out_binarized)
-);
+always @(posedge clk, reset) begin
+    if (reset) begin
+        conv_out_tmp <= 8'b00000000;
+    end
+    else begin
+        if (mode) begin
+            conv_out_tmp <= pixel_1 + pixel_3 - 4*pixel_4 + pixel_5 + pixel_7;
+            conv_out_tmp_sum_of_positive <= pixel_1 + pixel_3 + pixel_5 + pixel_7;
+            conv_out_tmp_sum_of_negative <= 4*pixel_4;
+        end
+        else begin
+            conv_out_tmp <= pixel_0 + 2*pixel_1 + pixel_2 + 2*pixel_3 - 12*pixel_4 + 2*pixel_5 + pixel_6 + 2*pixel_7 + pixel_8;
+            conv_out_tmp_sum_of_positive <= pixel_0 + 2*pixel_1 + pixel_2 + 2*pixel_3 + 2*pixel_5 + pixel_6 + 2*pixel_7 + pixel_8;
+            conv_out_tmp_sum_of_negative <= 12*pixel_4;
+        end
+    end
+end
 
 endmodule
--- Hardware/ImageFilterSystem/submodules/GrayscaleConverter.v
+++ Hardware/ImageFilterSystem/submodules/GrayscaleConverter.v
@@ -2,6 +2,9 @@ module GrayscaleConverter #(
     parameter BIT_PER_PIXEL = 8,
     parameter NUM_PIXELS = 9
 ) (
+    input
+        clk,
+        reset,
     input wire[BIT_PER_PIXEL-1:0]
         pixel_0_red,
         pixel_0_green,
@@ -30,7 +33,7 @@ module GrayscaleConverter #(
         pixel_8_red,
         pixel_8_green,
         pixel_8_blue,
-    output wire[BIT_PER_PIXEL-1:0]
+    output reg[BIT_PER_PIXEL-1:0]
         pixel_0_out,
         pixel_1_out,
         pixel_2_out,
@@ -42,13 +45,23 @@ module GrayscaleConverter #(
         pixel_8_out
 );
 
-// parameter
-parameter TMP_WIRE_WIDTH = 10;  // 10 bit is the minimum required width to store the sum of three 8 bit numbers
-
+// local parameter definitions
+localparam TMP_WIRE_WIDTH = 10;  // 10 bit is the minimum required width to store the sum of three 8 bit numbers
 
 // set of 10 bit wires that temporarily store the value of pixel_red + pixel_green + pixel_blue
 wire [TMP_WIRE_WIDTH-1:0] pixelwise_sums [NUM_PIXELS-1:0];
 wire [TMP_WIRE_WIDTH-1:0] pixelwise_means [NUM_PIXELS-1:0];
+
+// wires to temporarily store the output value
+wire [BIT_PER_PIXEL-1:0] pixel_0_out_tmp,
+wire [BIT_PER_PIXEL-1:0] pixel_1_out_tmp,
+wire [BIT_PER_PIXEL-1:0] pixel_2_out_tmp,
+wire [BIT_PER_PIXEL-1:0] pixel_3_out_tmp,
+wire [BIT_PER_PIXEL-1:0] pixel_4_out_tmp,
+wire [BIT_PER_PIXEL-1:0] pixel_5_out_tmp,
+wire [BIT_PER_PIXEL-1:0] pixel_6_out_tmp,
+wire [BIT_PER_PIXEL-1:0] pixel_7_out_tmp,
+wire [BIT_PER_PIXEL-1:0] pixel_8_out_tmp;
 
 // calculate pixel-wise sum
 assign pixelwise_sums[0] = pixel_0_red + pixel_0_green + pixel_0_blue;
@@ -73,14 +86,38 @@ assign pixelwise_means[7] = pixelwise_sums[7] / 3;
 assign pixelwise_means[8] = pixelwise_sums[8] / 3;
 
 // assign to the output
-assign pixel_0_out = pixelwise_means[0][BIT_PER_PIXEL-1:0];
-assign pixel_1_out = pixelwise_means[1][BIT_PER_PIXEL-1:0];
-assign pixel_2_out = pixelwise_means[2][BIT_PER_PIXEL-1:0];
-assign pixel_3_out = pixelwise_means[3][BIT_PER_PIXEL-1:0];
-assign pixel_4_out = pixelwise_means[4][BIT_PER_PIXEL-1:0];
-assign pixel_5_out = pixelwise_means[5][BIT_PER_PIXEL-1:0];
-assign pixel_6_out = pixelwise_means[6][BIT_PER_PIXEL-1:0];
-assign pixel_7_out = pixelwise_means[7][BIT_PER_PIXEL-1:0];
-assign pixel_8_out = pixelwise_means[8][BIT_PER_PIXEL-1:0];
+assign pixel_0_out_tmp = pixelwise_means[0][BIT_PER_PIXEL-1:0];
+assign pixel_1_out_tmp = pixelwise_means[1][BIT_PER_PIXEL-1:0];
+assign pixel_2_out_tmp = pixelwise_means[2][BIT_PER_PIXEL-1:0];
+assign pixel_3_out_tmp = pixelwise_means[3][BIT_PER_PIXEL-1:0];
+assign pixel_4_out_tmp = pixelwise_means[4][BIT_PER_PIXEL-1:0];
+assign pixel_5_out_tmp = pixelwise_means[5][BIT_PER_PIXEL-1:0];
+assign pixel_6_out_tmp = pixelwise_means[6][BIT_PER_PIXEL-1:0];
+assign pixel_7_out_tmp = pixelwise_means[7][BIT_PER_PIXEL-1:0];
+assign pixel_8_out_tmp = pixelwise_means[8][BIT_PER_PIXEL-1:0];
+
+always @(posedge clk, reset) begin
+    if (reset) begin
+        pixel_0_out <= 8'h00;
+        pixel_1_out <= 8'h00;
+        pixel_2_out <= 8'h00;
+        pixel_3_out <= 8'h00;
+        pixel_4_out <= 8'h00;
+        pixel_5_out <= 8'h00;
+        pixel_6_out <= 8'h00;
+        pixel_7_out <= 8'h00;
+        pixel_8_out <= 8'h00;
+    end else begin
+        pixel_0_out <= pixel_0_out_tmp;
+        pixel_1_out <= pixel_1_out_tmp;
+        pixel_2_out <= pixel_2_out_tmp;
+        pixel_3_out <= pixel_3_out_tmp;
+        pixel_4_out <= pixel_4_out_tmp;
+        pixel_5_out <= pixel_5_out_tmp;
+        pixel_6_out <= pixel_6_out_tmp;
+        pixel_7_out <= pixel_7_out_tmp;
+        pixel_8_out <= pixel_8_out_tmp;
+    end
+end
 
 endmodule
